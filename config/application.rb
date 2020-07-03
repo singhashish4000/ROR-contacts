7f02c9ef318c30c654bf7df811f9ecabd2edc0a5
Ashish Singh <ashish.singh@bombayshirts.com>
2020-07-04 00:18:57 +0530

    added support for portuguese and english language

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Contacts
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:en, :pt]
+    config.i18n.default_locale = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
